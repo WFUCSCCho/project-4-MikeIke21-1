dd43241f0736ce0813c8e3243dbbfd1f3c146dc2
mike iaccarino <iaccmj21@wfu.edu>
2024-12-02 10:20:47 -0500

    First Push

--- src/Proj4.java
+++ src/Proj4.java
@@ -1,5 +1,8 @@
+import com.sun.source.tree.AnyPatternTree;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -28,6 +31,171 @@ public class Proj4 {
         inputFileNameScanner.nextLine();
 
         // FINISH ME
+        ///*STEP 1 make 3 types of array LIST
+        ///* we have all the code for this in last project
+        ///*insert, search and delete functions
 
+        ArrayList dataArrList = new ArrayList();
+        ArrayList<player> playerArrList = new ArrayList();
+
+
+        //adding to the Arraylist
+        while (inputFileNameScanner.hasNextLine()) {
+            dataArrList.add(inputFileNameScanner.nextLine());
+        }
+        //making arraylist data player data
+
+        String playerStats;
+        String[] parsedStats;
+        for (int i = 0; i < numLines-1; i++) {
+            playerStats = (String) dataArrList.get(i);
+            parsedStats = playerStats.split(",");
+            if (parsedStats.length != 14){
+                //System.out.println(playerStats.length);
+            }
+            //removes the first line
+            else if (parsedStats[0].equals("overall_pick")){
+
+            }
+            else {
+                playerArrList.add(new player(Integer.parseInt(parsedStats[0]),Integer.parseInt(parsedStats[1]),parsedStats[2],parsedStats[3],parsedStats[4],Integer.parseInt(parsedStats[6]),
+                        Integer.parseInt(parsedStats[7]),Double.parseDouble(parsedStats[8]),Double.parseDouble(parsedStats[9]),Double.parseDouble(parsedStats[10]),Double.parseDouble(parsedStats[11]),Double.parseDouble(parsedStats[12]),Double.parseDouble(parsedStats[13])));
+            }
+
+
+        }
+
+
+        //SORTED ARR LIST
+        ArrayList<player> sortedArrList = new ArrayList();
+        sortedArrList.addAll(playerArrList);
+        Collections.sort(sortedArrList);
+
+        //random ARR List
+        ArrayList<player> randArrList = new ArrayList();
+        randArrList.addAll(playerArrList);
+        Collections.shuffle(randArrList);
+
+        //REVERSE ARR LIST
+        ArrayList<player> revArrList = new ArrayList();
+        revArrList.addAll(playerArrList);
+        Collections.sort(revArrList, Collections.reverseOrder());
+
+        ///*sanity check
+        //System.out.println(sortedArrList);
+        //System.out.println(randArrList);
+        //System.out.println(revArrList);
+
+        SeparateChainingHashTable<player> sortedHash = new SeparateChainingHashTable<>();
+        SeparateChainingHashTable<player> randHash = new SeparateChainingHashTable<>();
+        SeparateChainingHashTable<player> revHash = new SeparateChainingHashTable<>();
+
+
+
+        ///* insert functions timing
+        //Sorted Insert time
+        long insertSortStart = System.nanoTime();
+        for (int i = 0; i < sortedArrList.size()-1; i++) {
+            sortedHash.insert(sortedArrList.get(i));
+        }
+        long insertSortEnd = System.nanoTime();
+
+        //Shuffled insert time
+        long insertRandStart = System.nanoTime();
+        for (int i = 0; i < randArrList.size()-1; i++) {
+            randHash.insert(randArrList.get(i));
+        }
+
+        long insertRandEnd = System.nanoTime();
+
+        //Reverse insert time 
+        long insertRevStart = System.nanoTime();
+        for (int i = 0; i < revArrList.size()-1; i++) {
+            revHash.insert(revArrList.get(i));
+        }
+
+        long insertRevEnd = System.nanoTime();
+
+        //differences
+
+        long sortedInsertTime = insertSortEnd-insertSortStart;
+        long randInsertTime = insertRandEnd-insertRandStart;
+        long revInsertTime = insertRevEnd-insertRevStart;
+
+        //uploading to file
+
+
+        ///* search functions timing
+        long searchSortStart = System.nanoTime();
+        //search for the last item in the last
+        sortedHash.contains(sortedArrList.get(sortedArrList.size() - 1));
+        long searchSortEnd = System.nanoTime();
+
+        long searchRandStart = System.nanoTime();
+        randHash.contains(randArrList.get(randArrList.size() - 1));
+        long searchRandEnd= System.nanoTime();
+
+        long searchRevStart = System.nanoTime();
+        revHash.contains(revArrList.get(revArrList.size() - 1));
+        long searchRevEnd = System.nanoTime();
+
+        long searchSortTime = searchSortEnd-searchSortStart;
+        long searchRandTime = searchRandEnd-searchRandStart;
+        long searchRevTime = searchRevEnd-searchRevStart;
+
+
+        //uploading to file
+
+
+
+        ///* delete functions timing
+
+        long deleteSortStart = System.nanoTime();
+        for (int i = 0; i < sortedArrList.size()-1; i++) {
+            sortedHash.remove(sortedArrList.get(i));
+        }
+        long deleteSortEnd = System.nanoTime();
+
+        long deleteRandStart = System.nanoTime();
+        for (int i = 0; i < randArrList.size()-1; i++) {
+            randHash.remove(randArrList.get(i));
+        }
+        long deleteRandEnd = System.nanoTime();
+
+        long deleteRevStart = System.nanoTime();
+        revHash.remove(revArrList.get(revArrList.size() - 1));
+        long deleteRevEnd = System.nanoTime();
+
+        long deleteSortTime = deleteSortEnd-deleteSortStart;
+        long deleteRandTime = deleteRandEnd-deleteRandStart;
+        long deleteRevTime = deleteRevEnd-deleteRevStart;
+
+        ///* writing to file
+        writeTofile( "==========RUNTIMES==========", "analysis.txt");
+        writeTofile("NODES: " + numLines, "analysis.txt");
+        writeTofile( "-----INSERTS-----", "analysis.txt");
+        writeTofile( "Sorted: " + sortedInsertTime, "analysis.txt");
+        writeTofile( "Shuffled: " + randInsertTime, "analysis.txt");
+        writeTofile( "Reversed: " + revInsertTime, "analysis.txt");
+        writeTofile( "-----SEARCHES-----", "analysis.txt");
+        writeTofile( "Sorted: " + searchSortTime, "analysis.txt");
+        writeTofile( "Shuffled: " + searchRandTime, "analysis.txt");
+        writeTofile( "Reversed: " + searchRevTime, "analysis.txt");
+        writeTofile("-----DELETIONS-----", "analysis.txt");
+        writeTofile( "Sorted: " + deleteSortTime, "analysis.txt");
+        writeTofile( "Shuffled: " + deleteRandTime, "analysis.txt");
+        writeTofile( "Reversed: " + deleteRevTime, "analysis.txt");
+
+
+
+
+
+    }
+
+    //FILEWRITER
+    private static void writeTofile(String content, String filePath) throws IOException {
+        FileWriter file  = new FileWriter(filePath, true);
+        file.write(content + System.lineSeparator());
+        file.close();
     }
 }
